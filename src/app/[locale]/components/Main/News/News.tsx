3edc369722b95ffd2a804768f7619e8e6d1b2394
Irka-Bodnariuk <bodnariuk.iryna1@gmail.com>
2024-02-27 19:25:55 +0200

    feat: add reviews on main page

---
+++ src/app/[locale]/components/Main/News/News.tsx
@@ -0,0 +1,9 @@
+const News = () => {
+  return (
+    <div>
+      <h2>News</h2>
+    </div>
+  );
+};
+
+export default News;
--- src/app/[locale]/page.jsx
+++ src/app/[locale]/page.jsx
@@ -1,12 +1,12 @@
 import ExampleClientComponent from "../../../components/ExampleClientComponent";
 import initTranslations from "../i18n";
-import Link from "next/link";
 import TranslationsProvider from "../../../components/TranslationsProvider";
 import LanguageChanger from "../../../components/LanguageChanger";
 import Hero from "./components/Main/Hero/Hero";
 import WhatWeDo from "./components/Main/WhatWeDo/WhatWeDo";
 import Projects from "./components/Main/Projects/Projects";
 import Reviews from "./components/Main/Reviews/Reviews";
+import News from "./components/Main/News/News";
 const i18nNamespaces = ["home", "common"];
 
 export default async function Home({ params: { locale } }) {
@@ -18,13 +18,16 @@ export default async function Home({ params: { locale } }) {
       locale={locale}
       resources={resources}
     >
-      <div>
+      <div className="container">
         <LanguageChanger />
         <ExampleClientComponent />
-        <Hero />
-        <WhatWeDo />
-        <Projects />
-        {/* <Reviews /> */}
+        <div style={{ display: "flex", flexDirection: "column", gap: "120px" }}>
+          <Hero />
+          <WhatWeDo />
+          <Projects />
+          <Reviews />
+          {/* <News /> */}
+        </div>
       </div>
     </TranslationsProvider>
   );

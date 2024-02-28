fe3f625ec8870a7fa6198d7b06204bfaf660c453
ImVladislav <nafanya102@gmail.com>
2024-02-28 22:37:04 +0200

    asd

--- src/app/[locale]/components/Main/SafetyRules.jsx
+++ src/app/[locale]/components/Main/SafetyRules.jsx
@@ -36,35 +36,12 @@
 
 // export default SafetyRules;
 
-"use client";
+// "use client";
 
-import { useLocale } from "next-intl";
-import { useRouter } from "next/navigation";
-import { ChangeEvent, useTransition } from "react";
+import React from "react";
 
-export default function LocalSwitcher() {
-  const [isPending, startTransition] = useTransition();
-  const router = useRouter();
-  const localActive = useLocale();
+const SafetyRules = () => {
+  return <div></div>;
+};
 
-  const onSelectChange = (e: ChangeEvent<HTMLSelectElement>) => {
-    const nextLocale = e.target.value;
-    startTransition(() => {
-      router.replace(`/${nextLocale}`);
-    });
-  };
-  return (
-    <label className="border-2 rounded">
-      <p className="sr-only">change language</p>
-      <select
-        defaultValue={localActive}
-        className="bg-transparent py-2"
-        onChange={onSelectChange}
-        disabled={isPending}
-      >
-        <option value="en">English</option>
-        <option value="id">Indonesian</option>
-      </select>
-    </label>
-  );
-}
+export default SafetyRules;

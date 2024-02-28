53eace0c450bc15b13bb89ef47b45495d3ee2b38
ImVladislav <nafanya102@gmail.com>
2024-02-28 21:44:12 +0200

    fix

--- src/app/[locale]/page.jsx
+++ src/app/[locale]/page.jsx
@@ -1,31 +1,17 @@
 import ExampleClientComponent from "../../../components/ExampleClientComponent";
-import initTranslations from "../i18n";
-import Link from "next/link";
-import TranslationsProvider from "../../../components/TranslationsProvider";
 import LanguageChanger from "../../../components/LanguageChanger";
 import Hero from "./components/Main/Hero/Hero";
 import WhatWeDo from "./components/Main/WhatWeDo/WhatWeDo";
 import Projects from "./components/Main/Projects/Projects";
 import Reviews from "./components/Main/Reviews/Reviews";
-const i18nNamespaces = ["home", "common"];
 
-export default async function Home({ params: { locale } }) {
-  // const { t } = await initTranslations(locale, ["home", "common"]);
-  const { t, resources } = await initTranslations(locale, i18nNamespaces);
+export default async function Home() {
   return (
-    <TranslationsProvider
-      namespaces={i18nNamespaces}
-      locale={locale}
-      resources={resources}
-    >
-      <div>
-        <LanguageChanger />
-        <ExampleClientComponent />
-        <Hero />
-        <WhatWeDo />
-        <Projects />
-        {/* <Reviews /> */}
-      </div>
-    </TranslationsProvider>
+    <div>
+      <Hero />
+      <WhatWeDo />
+      <Projects />
+      {/* <Reviews /> */}
+    </div>
   );
 }

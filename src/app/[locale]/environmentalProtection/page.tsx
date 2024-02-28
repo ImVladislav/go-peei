4ac085ea9b90f685812200a161c0a253f68d63f4
ImVladislav <nafanya102@gmail.com>
2024-02-28 22:30:32 +0200

    fix

--- src/app/[locale]/environmentalProtection/page.tsx
+++ src/app/[locale]/environmentalProtection/page.tsx
@@ -1,81 +1,81 @@
-import Image from "next/image";
-import React from "react";
-import bg from "../../../public/environmentalProtection/EnvironmentalProtectionBG.jpg";
-import environmentalProtection from "../../../public/environmentalProtection/EnvironmentalProtection1.jpg";
-const EnvironmentalProtection = () => {
-  return (
-    <div style={{ backgroundImage: `url(${bg.src})`, backgroundSize: "cover" }}>
-      <div className="container">
-        <h3 className="about__text-block-title">
-          Наша Роль у Збереженні Навколишнього Середовища  
-        </h3>
-        <section
-          style={{
-            alignItems: "center",
-            display: "flex",
-            flexDirection: "row-reverse",
-          }}
-        >
-          <Image
-            className="about__images"
-            src={environmentalProtection}
-            alt={"about us image"}
-          />
-          <div
-            style={{ backgroundColor: "#ffffff85", padding: "0px 20px" }}
-            className="about__text-block"
-          >
-            {/* <h3 className="about__text-block-title">
-              Наша Роль у Збереженні Навколишнього Середовища  
-            </h3> */}
-            <ul>
-              <li className="about__text-block-item">
-                <p style={{ fontWeight: "900" }} className="about__text">
-                  Наша організація приділяє особливу увагу питанням охорони
-                  довкілля, розуміючи важливість нашого внеску в збереження
-                  навколишнього середовища. Ми розглядаємо проблему екології як
-                  необхідний аспект нашої діяльності та визнаємо, що маємо
-                  безпосередню відповідальність за стан навколишнього
-                  середовища.
-                </p>
-              </li>
-              <li className="about__text-block-item">
-                <p style={{ fontWeight: "900" }} className="about__text">
-                  У наших ініціативах головними компонентами є використання
-                  інноваційних технологій та досягнення енергоефективності. Ми
-                  систематично впроваджуємо нові технології, спрямовані на
-                  зменшення викидів, оптимізацію енергоспоживання та підвищення
-                  ефективності використання відновлювальних джерел енергії
-                </p>
-              </li>
-              <li className="about__text-block-item">
-                <p style={{ fontWeight: "900" }} className="about__text">
-                  Ми активно працюємо над підтримкою переробки пластикових
-                  відходів, використовуючи передові технології та процеси для
-                  перетворення їх у вторинні матеріали та корисні предмети,
-                  сприяючи тим самим зменшенню кількості сміття та уникненню
-                  його негативного впливу на природу
-                </p>
-              </li>
-              <li className="about__text-block-item">
-                <p style={{ fontWeight: "900" }} className="about__text">
-                  Наша громадська організація віддана сталому розвитку та
-                  охороні довкілля. Ми враховуємо міжнародні стандарти щодо
-                  охорони довкілля та прагнемо перевершувати їх, створюючи
-                  збалансований підхід до розв&lsquo;язання екологічних проблем.
-                  Наша мета - відновлення та підтримка екосистем, а також
-                  активна робота зі зменшення відходів та заохочення
-                  використання вторинної сировини як ключового елементу в цьому
-                  процесі, з метою забезпечення сталого розвитку та збереження
-                  природних ресурсів для майбутніх поколінь..
-                </p>
-              </li>
-            </ul>
-          </div>
-        </section>
-      </div>
-    </div>
-  );
-};
+// import Image from "next/image";
+// import React from "react";
+// // import bg from "../../../public/environmentalProtection/EnvironmentalProtectionBG.jpg";
+// // import environmentalProtection from "../../../public/environmentalProtection/EnvironmentalProtection1.jpg";
+// const EnvironmentalProtection = () => {
+//   return (
+//     // <div style={{ backgroundImage: `url(${bg.src})`, backgroundSize: "cover" }}>
+//       <div className="container">
+//         <h3 className="about__text-block-title">
+//           Наша Роль у Збереженні Навколишнього Середовища
+//         </h3>
+//         <section
+//           style={{
+//             alignItems: "center",
+//             display: "flex",
+//             flexDirection: "row-reverse",
+//           }}
+//         >
+//           {/* <Image
+//             className="about__images"
+//             src={environmentalProtection}
+//             alt={"about us image"}
+//           /> */}
+//           <div
+//             style={{ backgroundColor: "#ffffff85", padding: "0px 20px" }}
+//             className="about__text-block"
+//           >
+//             {/* <h3 className="about__text-block-title">
+//               Наша Роль у Збереженні Навколишнього Середовища
+//             </h3> */}
+//             <ul>
+//               <li className="about__text-block-item">
+//                 <p style={{ fontWeight: "900" }} className="about__text">
+//                   Наша організація приділяє особливу увагу питанням охорони
+//                   довкілля, розуміючи важливість нашого внеску в збереження
+//                   навколишнього середовища. Ми розглядаємо проблему екології як
+//                   необхідний аспект нашої діяльності та визнаємо, що маємо
+//                   безпосередню відповідальність за стан навколишнього
+//                   середовища.
+//                 </p>
+//               </li>
+//               <li className="about__text-block-item">
+//                 <p style={{ fontWeight: "900" }} className="about__text">
+//                   У наших ініціативах головними компонентами є використання
+//                   інноваційних технологій та досягнення енергоефективності. Ми
+//                   систематично впроваджуємо нові технології, спрямовані на
+//                   зменшення викидів, оптимізацію енергоспоживання та підвищення
+//                   ефективності використання відновлювальних джерел енергії
+//                 </p>
+//               </li>
+//               <li className="about__text-block-item">
+//                 <p style={{ fontWeight: "900" }} className="about__text">
+//                   Ми активно працюємо над підтримкою переробки пластикових
+//                   відходів, використовуючи передові технології та процеси для
+//                   перетворення їх у вторинні матеріали та корисні предмети,
+//                   сприяючи тим самим зменшенню кількості сміття та уникненню
+//                   його негативного впливу на природу
+//                 </p>
+//               </li>
+//               <li className="about__text-block-item">
+//                 <p style={{ fontWeight: "900" }} className="about__text">
+//                   Наша громадська організація віддана сталому розвитку та
+//                   охороні довкілля. Ми враховуємо міжнародні стандарти щодо
+//                   охорони довкілля та прагнемо перевершувати їх, створюючи
+//                   збалансований підхід до розв&lsquo;язання екологічних проблем.
+//                   Наша мета - відновлення та підтримка екосистем, а також
+//                   активна робота зі зменшення відходів та заохочення
+//                   використання вторинної сировини як ключового елементу в цьому
+//                   процесі, з метою забезпечення сталого розвитку та збереження
+//                   природних ресурсів для майбутніх поколінь..
+//                 </p>
+//               </li>
+//             </ul>
+//           </div>
+//         </section>
+//       </div>
+//     </div>
+//   );
+// };
 
-export default EnvironmentalProtection;
+// export default EnvironmentalProtection;

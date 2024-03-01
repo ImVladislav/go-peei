6f078849505c5de5d6c1b5beaf867320b93d1598
Irka-Bodnariuk <bodnariuk.iryna1@gmail.com>
2024-03-01 16:09:28 +0200

    fix: change page

--- src/app/[locale]/page.jsx
+++ src/app/[locale]/page.jsx
@@ -7,15 +7,18 @@ import WhatWeDo from "./components/Main/WhatWeDo/WhatWeDo";
 import Projects from "./components/Main/Projects/Projects";
 import Reviews from "./components/Main/Reviews/Reviews";
 import News from "./components/Main/News/News";
+import styles from "./page.module.scss";
 
 export default async function Home() {
   return (
     <div className="container">
-      <Hero />
-      <WhatWeDo />
-      <Projects />
-      <Reviews />
-      <News />
+      <div className={styles.wrap}>
+        <Hero />
+        <WhatWeDo />
+        <Projects />
+        <Reviews />
+        <News />
+      </div>
     </div>
   );
 }

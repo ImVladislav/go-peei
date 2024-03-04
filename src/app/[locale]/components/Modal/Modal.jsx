c7258173a717a6e1f5fb9b7f15293402ea525032
ImVladislav <nafanya102@gmail.com>
2024-03-04 22:58:58 +0200

    Update Modal.jsx

--- src/app/[locale]/components/Modal/Modal.jsx
+++ src/app/[locale]/components/Modal/Modal.jsx
@@ -1,31 +1,38 @@
-"use client"
-import { useState, useEffect } from "react"
-import s from "./Modal.module.scss"
+"use client";
+import { useState, useEffect } from "react";
+import s from "./Modal.module.scss";
+import { ReactDOM } from "react-dom";
 
 export default function Modal({ show, onClose, children }) {
-    const [isBrowser, setBrowser] = useState(false)
-    
-    useEffect(() => {
-        setBrowser(true)
-    }, [])
-    
-    const handleClose = (e) => {
-        e.preventDefault()
-        onClose()
-    }
+  const [isBrowser, setBrowser] = useState(false);
 
-    const modalContent = show ? (
-        <div className={s.overlay}>
-            <div className={s.modal}>
-                <div className={s.header}>
-                    <a href="#" onClick={handleClose}>
-                        <button className="btn">close</button>
-                    </a>
-                </div>
-                <div className={s.body}>
-                    {children}
-                </div>
-            </div>
+  useEffect(() => {
+    setBrowser(true);
+  }, []);
+
+  const handleClose = (e) => {
+    e.preventDefault();
+    onClose();
+  };
+
+  const modalContent = show ? (
+    <div className={s.overlay}>
+      <div className={s.modal}>
+        <div className={s.header}>
+          <a href="#" onClick={handleClose}>
+            <button className="btn">close</button>
+          </a>
         </div>
-    )
+        <div className={s.body}>{children}</div>
+      </div>
+    </div>
+  ) : null;
+  if (isBrowser) {
+    return ReactDOM.createPortal(
+      modalContent,
+      document.getElementById("modal-root")
+    );
+  } else {
+    return null;
+  }
 }

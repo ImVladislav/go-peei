8f192d38dbabfaf68d864015aadfffbaeba8b074
Vladislav Zavertaylo <102727549+vladislavzaver91@users.noreply.github.com>
2024-03-18 16:00:41 +0200

    feat: page about & components. Adds variables

--- next/src/app/[locale]/components/About/AboutUs/AboutUs.tsx
+++ next/src/app/[locale]/components/About/AboutUs/AboutUs.tsx
@@ -1,3 +1,33 @@
+'use client'
+
+import Image from 'next/image'
+import Title from '../../Title/Title'
+import { ABOUT_US } from './about-us.data'
+import styles from './aboutUs.module.scss'
+
+const AboutUs = () => {
+  return (
+<section className={styles.section}>
+  <Title width={130}>Про нас</Title>
+  <p className={styles.text}><span className={styles.accent_text}>“Платформа еко-енергетичних ініціатив”</span> об'єднує зусилля активістів для сприяння сталому та екологічно безпечному майбутньому. За допомогою інноваційних технологій та співпраці з експертами у сфері відновлення ресурсів, ми прагнемо до зменшення негативного впливу відходів на наше середовище. </p>
+  <ul className={styles.list}>
+  {ABOUT_US.map((item, index) => (
+    <li key={index} className={styles.item}>
+      <Image src={item.icon} alt='Icon' width={17} height={30} />
+      <p className={styles.item__desc}>{item.desc}</p>
+    </li>
+  ))}
+  </ul>
+  <div className={styles.bg_img}>
+    <Image src='/about/about_bgimg.png' alt='leaf' width={762} height={690} />
+  </div>
+  
+</section>
+  )
+}
+
+export default AboutUs;
+
 // import Image from "next/image";
 // import about1 from "../../../public/about/aboutus1image.jpg";
 

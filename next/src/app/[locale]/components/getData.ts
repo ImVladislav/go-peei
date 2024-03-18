486a6f07e5d4e6f6249b815dcee46552ed6bd6ea
ImVladislav <nafanya102@gmail.com>
2024-03-18 19:53:44 +0200

    Update getData.ts

--- next/src/app/[locale]/components/getData.ts
+++ next/src/app/[locale]/components/getData.ts
@@ -1,14 +1,15 @@
 import { notFound } from "next/navigation";
 
 const { BASE_URL } = process.env;
-
+const { PROD_URL } = process.env;
+const url = PROD_URL || BASE_URL;
 export async function getData(name: string) {
-  const res = await fetch(`${BASE_URL}/api/${name}`, { cache: "no-store" });
+  const res = await fetch(`${url}/api/${name}`, { cache: "no-store" });
   if (!res.ok) return notFound();
   return res.json();
 }
 export async function getDataId(name: string, id: string) {
-  const res = await fetch(`${BASE_URL}/api/${name}/${id}`, {
+  const res = await fetch(`${url}/api/${name}/${id}`, {
     cache: "no-store",
   });
   if (!res.ok) return notFound();

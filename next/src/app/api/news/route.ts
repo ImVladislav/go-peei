456ae0728e96097e7a7bdc46b867bce17056ad6d
ImVladislav <nafanya102@gmail.com>
2024-03-18 18:36:06 +0200

    Update route.ts

--- next/src/app/api/news/route.ts
+++ next/src/app/api/news/route.ts
@@ -1,39 +1,39 @@
-import { NextResponse } from "next/server";
+// import { NextResponse } from "next/server";
 
-import db from "../../../../libs/db";
-import News from "../../../../models/News";
+// import db from "../../../../libs/db";
+// import News from "../../../../models/News";
 
-// get news
-export const GET = async (request: Request) => {
-  await db.connect();
-  try {
-    const items = await News.find();
-    return new NextResponse(JSON.stringify(items), {
-      status: 200,
-    });
-  } catch (error) {
-    return new NextResponse("Error in fetching news" + error, {
-      status: 500,
-    });
-  }
-};
-// add news
-export const POST = async (request: Request) => {
-  await db.connect();
-  try {
-    const body = await request.json();
+// // get news
+// export const GET = async (request: Request) => {
+//   await db.connect();
+//   try {
+//     const items = await News.find();
+//     return new NextResponse(JSON.stringify(items), {
+//       status: 200,
+//     });
+//   } catch (error) {
+//     return new NextResponse("Error in fetching news" + error, {
+//       status: 500,
+//     });
+//   }
+// };
+// // add news
+// export const POST = async (request: Request) => {
+//   await db.connect();
+//   try {
+//     const body = await request.json();
 
-    // if (body.firstNew === true) {
-    //   const items = await News.find({ firstNew });
-    // }
+//     // if (body.firstNew === true) {
+//     //   const items = await News.find({ firstNew });
+//     // }
 
-    const newNews = await News.create({ body });
-    return new NextResponse(JSON.stringify(newNews), {
-      status: 201,
-    });
-  } catch (error) {
-    return new NextResponse("Error add new news" + error, {
-      status: 500,
-    });
-  }
-};
+//     const newNews = await News.create({ body });
+//     return new NextResponse(JSON.stringify(newNews), {
+//       status: 201,
+//     });
+//   } catch (error) {
+//     return new NextResponse("Error add new news" + error, {
+//       status: 500,
+//     });
+//   }
+// };

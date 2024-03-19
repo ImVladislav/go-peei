3bc259799ae30a03bf435a687a0156edc8933fbb
Irka-Bodnariuk <bodnariuk.iryna1@gmail.com>
2024-03-19 18:35:37 +0200

    fix: change route docs and change page statut

--- next/src/app/api/docs/route.ts
+++ next/src/app/api/docs/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 import db from "../../../../libs/db";
 import Docs from "../../../../models/Docs";
 
-// get news
+// get Docs
 export const GET = async (request: Request) => {
   await db.connect();
   try {
@@ -12,7 +12,7 @@ export const GET = async (request: Request) => {
       status: 200,
     });
   } catch (error) {
-    return new NextResponse("Error in fetching news" + error, {
+    return new NextResponse("Error in fetching documents" + error, {
       status: 500,
     });
   }

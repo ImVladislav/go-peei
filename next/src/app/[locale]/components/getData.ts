6166373aaed0713dda4bca831588f7fb4684a695
ImVladislav <nafanya102@gmail.com>
2024-03-21 20:31:49 +0200

    fix

--- next/src/app/[locale]/components/getData.ts
+++ next/src/app/[locale]/components/getData.ts
@@ -5,7 +5,7 @@ const { PROD_URL } = process.env;
 
 // const { NEXT_HOST } = process.env;
 
-const url = PROD_URL || BASE_URL;
+const url = process.env.NODE_ENV === "production" ? PROD_URL : BASE_URL;
 export async function getData(name: string) {
   const res = await fetch(`${url}/api/${name}`, { cache: "no-store" });
   if (!res.ok) return notFound();

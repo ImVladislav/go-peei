742394fc859e81b9d614354c48ba65c35e1af8e2
vladislavzaver91 <vladislavzaver91@gmail.com>
2024-04-09 14:34:28 +0300

    feat: add news item page

--- next/src/app/[locale]/news/[id]/page.tsx
+++ next/src/app/[locale]/news/[id]/page.tsx
@@ -1,36 +1,40 @@
-import Image from "next/image";
+import Image from 'next/image'
 
-import Title from "../../components/Title/Title";
-import { getDataId } from "../../components/getData";
-import { newsItem } from "@/app/types";
+import { newsItem } from '@/app/types'
+// import Title from '../../components/Title/Title'
+import { getDataId } from '../../components/getData'
 
-import styles from "../news.module.scss";
+import styles from '../news.module.scss'
 
 const NewsDetails = async ({ params }: { params: { id: string } }) => {
-  const { id } = params;
-  const data: newsItem = await getDataId("news", id);
-  return (
-    <div className="container">
-      <div className={styles.card}>
-        <div className={styles.card__img}>
-          <Image
-            style={{
-              objectFit: "cover",
-              objectPosition: "center",
-              borderRadius: "10px",
-            }}
-            src={data.imageSrc}
-            alt={data.title}
-            width={1000}
-            height={500}
-          />
-        </div>
-        <Title width={1100}> {data.title}</Title>
-        <p className={styles.card__description}>{data.description}</p>
-        <p className={styles.card__data}>{data.date}</p>
-      </div>
-    </div>
-  );
-};
+	const { id } = params
+	const data: newsItem = await getDataId('news', id)
+	return (
+		<div className='container'>
+			<div className={styles.card}>
+				<div className={styles.card__img}>
+					<Image
+						style={{
+							objectFit: 'cover',
+							objectPosition: 'center',
+						}}
+						src={data.imageSrc}
+						alt={data.title}
+						width={360}
+						height={180}
+					/>
+				</div>
+				<div className={styles.card__wrapDescription}>
+					<h3 className={styles.card__title}>{data.title}</h3>
+					<p className={styles.card__description}>{data.description}</p>
+					<p className={styles.card__data}>
+						<span className={styles.card__dataTitle}>Опубліковано: </span>
+						{data.date}
+					</p>
+				</div>
+			</div>
+		</div>
+	)
+}
 
-export default NewsDetails;
+export default NewsDetails

8c234e7a68bce266224b10a5405c9f44a1ecafd4
Irka-Bodnariuk <bodnariuk.iryna1@gmail.com>
2024-04-19 19:07:22 +0300

    feat: add WorkWithTeenagersPage

--- next/src/app/[locale]/activities/work-with-teenagers/page.tsx
+++ next/src/app/[locale]/activities/work-with-teenagers/page.tsx
@@ -1,7 +1,59 @@
-import React from "react";
+import Title from "../../components/Title/Title";
+import styles from "./workWithTeenagers.module.scss";
+import Translator from "../../components/translator/Translator";
+import Image from "next/image";
+import Button from "../../components/Button/Button";
 
 const WorkWithTeenagersPage = () => {
-  return <div>WorkWithTeenagersPage</div>;
+  return (
+    <div className={styles.section}>
+      <div className="container">
+        <Title>
+          <Translator>work</Translator>
+          <span className={styles.accentTitle}>
+            <Translator>withYoungPeople</Translator>
+          </span>
+        </Title>
+        <div className={styles.contentTop}>
+          <div className={styles.imgContent}>
+            <div className={styles.wrapImg}>
+              <Image
+                className={styles.img}
+                src="/workWithTeenagers/environment-volunteer-teamwork-concept.jpg"
+                alt="img"
+                fill
+              />
+            </div>
+          </div>
+          <div className={styles.card}>
+            <div className={styles.textContent}>
+              <p className={styles.hello}>
+                <Translator>hello</Translator>
+              </p>
+              <div>
+                <p className={styles.description}>
+                  <Translator>joinTheTeamDescFirst</Translator>
+                </p>
+                <p className={styles.description}>
+                  <br />
+                  <Translator>joinTheTeamDescSecond</Translator>
+                </p>
+                <p className={styles.description}>
+                  <br />
+                  <Translator>joinTheTeamDescThird</Translator>
+                </p>
+              </div>
+              <Button>
+                <Translator>joinTheTeam</Translator>
+              </Button>
+            </div>
+          </div>
+        </div>
+        <div className={styles.contentCenter}></div>
+        <div className={styles.contentBottom}></div>
+      </div>
+    </div>
+  );
 };
 
 export default WorkWithTeenagersPage;

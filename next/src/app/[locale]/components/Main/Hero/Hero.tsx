0016a25527570814beec394928c631d71d728a7d
Irka-Bodnariuk <bodnariuk.iryna1@gmail.com>
2024-04-25 22:36:11 +0300

    fix: change styles in section hero and whatWeDo

--- next/src/app/[locale]/components/Main/Hero/Hero.tsx
+++ next/src/app/[locale]/components/Main/Hero/Hero.tsx
@@ -1,62 +1,76 @@
-"use client";
-
-import { useRouter } from "next/navigation";
 import Image from "next/image";
 
 import Button from "../../Button/Button";
-
-import styles from "./hero.module.scss";
 import Translator from "../../translator/Translator";
 
-const Hero = () => {
-  const router = useRouter();
+import styles from "./hero.module.scss";
+import Link from "next/link";
 
+const Hero = () => {
   return (
     <section className={styles.section}>
       <div className="container">
-        <h1 className="visually-hidden">
-          <Translator>environmentalSecurityEnergyIndependence</Translator>
-        </h1>
-        <div className={styles.inner}>
-          <div className={styles.column}>
-            <div className={styles.content}>
-              <p className={styles.slogan}>
-                <Translator>environmental</Translator>
-                <span>
-                  <Translator>security</Translator> -
-                </span>
-              </p>
-              <p className={styles.slogan}>
-                <span>
-                  <Translator>energy</Translator>
-                </span>
-                <Translator>independence</Translator>
-              </p>
+        <div style={{ position: "relative" }}>
+          <div>
+            <h1 className="visually-hidden">
+              <Translator>environmentalSecurityEnergyIndependence</Translator>
+            </h1>
+            <div className={styles.inner}>
+              <div className={styles.column}>
+                <div className={styles.sloganContainer}>
+                  <div className={styles.sloganInner}>
+                    <p className={styles.slogan}>
+                      <Translator>environmental</Translator>
+                    </p>
+                    <p className={styles.slogan}>
+                      <span>
+                        <Translator>security</Translator> -
+                      </span>
+                    </p>
+                  </div>
+                  <div className={styles.sloganInner}>
+                    <p className={styles.slogan}>
+                      <span>
+                        <Translator>energy</Translator>
+                      </span>
+                    </p>
+                    <p className={styles.slogan}>
+                      <Translator>independence</Translator>
+                    </p>
+                  </div>
+                </div>
+                <div className={styles.btnWrap}>
+                  <Button
+                    newStyles={styles.btn}
+                    isLink
+                    href="https://payhub.com.ua/#/service/98840"
+                  >
+                    <Translator>supportUs</Translator>
+                  </Button>
+                </div>
+              </div>
+              <div className={styles.img_wrap}>
+                <Image
+                  className={styles.img}
+                  src="/main/recycling-symbol-green-nature-isolated-white-background.jpg"
+                  alt="піктограма повторної переробки"
+                  fill
+                />
+              </div>
+              <div className={styles.btn_lastWrap}>
+                <Button
+                  newStyles={styles.btn_last}
+                  isLink
+                  href="https://payhub.com.ua/#/service/98840"
+                >
+                  <Translator>supportUs</Translator>
+                </Button>
+              </div>
             </div>
-
-            <Button
-              newStyles={styles.btn}
-              onClick={() =>
-                router.push("https://payhub.com.ua/#/service/98840")
-              }
-            >
-              <Translator>supportUs</Translator>
-            </Button>
           </div>
-          <div className={styles.img_wrap}>
-            <Image
-              className={styles.img}
-              src="/main/pic.png"
-              alt="піктограма повторної переробки"
-              fill
-            />
-          </div>
-          <Button
-            newStyles={styles.btn_last}
-            onClick={() => router.push("https://payhub.com.ua/#/service/98840")}
-          >
-            <Translator>supportUs</Translator>
-          </Button>
+          <Link href="/" className={styles.linkSecurity}>
+            Правила безпеки під час воєнного стану
+          </Link>
         </div>
       </div>
     </section>

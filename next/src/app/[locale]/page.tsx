ba466e437b0baf9d010a6e0069bfe7719ca53a49
vladislavzaver91 <vladislavzaver91@gmail.com>
2024-04-30 17:16:47 +0300

    feat: add btn scroll top, fixed news item

--- next/src/app/[locale]/page.tsx
+++ next/src/app/[locale]/page.tsx
@@ -1,5 +1,10 @@
 import { getData } from '../../../libs/getData'
 import { newsItem, projectsItem, reviewsItem } from '../types'
+import Hero from './components/Main/Hero/Hero'
+import News from './components/Main/News/News'
+import Projects from './components/Main/Projects/Projects'
+import Reviews from './components/Main/Reviews/Reviews'
+import WhatWeDo from './components/Main/WhatWeDo/WhatWeDo'
 
 import styles from './main.module.scss'
 

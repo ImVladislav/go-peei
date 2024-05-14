4432b284232475ca4ae45c54fd348d8c47d38f71
ImVladislav <nafanya102@gmail.com>
2024-05-14 18:35:54 +0300

    Update SwiperContainer.tsx

--- next/src/app/[locale]/components/Main/Reviews/SwiperContainer.tsx
+++ next/src/app/[locale]/components/Main/Reviews/SwiperContainer.tsx
@@ -10,14 +10,85 @@ import SingleReview from "./SingleReview";
 
 import styles from "./reviews.module.scss";
 
+// const SwiperContainer = ({ data }: { data: reviewsItem[] }) => {
+//   return (
+//     <Swiper
+//       style={{
+//         "--swiper-theme-color": "#0d490d",
+//         "--swiper-pagination-bullet-size": "20px",
+//         "--swiper-pagination-bottom": "0px",
+//         // " --swiper-pagination-bullet-horizontal-gap": "16px",
+//       }}
+//       slidesPerView={1.03}
+//       spaceBetween={12}
+//       grabCursor={true}
+//       loop={true}
+//       pagination={{
+//         dynamicBullets: true,
+//       }}
+//       autoplay={{
+//         delay: 500,
+//         pauseOnMouseEnter: true,
+//         disableOnInteraction: true,
+//         waitForTransition: true,
+//       }}
+//       speed={500}
+//       modules={[Autoplay, Pagination]}
+//       breakpoints={{
+//         425: {
+//           slidesPerView: 1.2,
+//         },
+//         500: {
+//           slidesPerView: 1.5,
+//         },
+//         600: {
+//           slidesPerView: 1.8,
+//         },
+//         768: {
+//           slidesPerView: 1.4,
+//           spaceBetween: 32,
+//         },
+//         900: {
+//           slidesPerView: 1.6,
+//         },
+
+//         1024: {
+//           slidesPerView: 1.9,
+//         },
+//         1100: {
+//           slidesPerView: 2,
+//         },
+//         1200: {
+//           slidesPerView: 2.2,
+//           spaceBetween: 56,
+//         },
+//         1300: {
+//           slidesPerView: 2.3,
+//         },
+//         1440: {
+//           slidesPerView: 2.5,
+//         },
+//       }}
+//       className={`mySwiper ${styles.swiper}`}
+//     >
+//       {data?.map((item) => (
+//         <SwiperSlide className={styles.swiper_slide} key={item._id}>
+//           <SingleReview review={item} />
+//         </SwiperSlide>
+//       ))}
+//     </Swiper>
+//   );
+// };
+
+// export default SwiperContainer;
 const SwiperContainer = ({ data }: { data: reviewsItem[] }) => {
   return (
     <Swiper
       style={{
-        "--swiper-theme-color": "#0d490d",
-        "--swiper-pagination-bullet-size": "20px",
-        "--swiper-pagination-bottom": "0px",
-        // " --swiper-pagination-bullet-horizontal-gap": "16px",
+        background: "#0d490d",
+        paginationBulletSize: "20px",
+        paginationBottom: "0px",
+        // paginationBulletHorizontalGap: "16px",
       }}
       slidesPerView={1.03}
       spaceBetween={12}
@@ -38,36 +109,7 @@ const SwiperContainer = ({ data }: { data: reviewsItem[] }) => {
         425: {
           slidesPerView: 1.2,
         },
-        500: {
-          slidesPerView: 1.5,
-        },
-        600: {
-          slidesPerView: 1.8,
-        },
-        768: {
-          slidesPerView: 1.4,
-          spaceBetween: 32,
-        },
-        900: {
-          slidesPerView: 1.6,
-        },
-
-        1024: {
-          slidesPerView: 1.9,
-        },
-        1100: {
-          slidesPerView: 2,
-        },
-        1200: {
-          slidesPerView: 2.2,
-          spaceBetween: 56,
-        },
-        1300: {
-          slidesPerView: 2.3,
-        },
-        1440: {
-          slidesPerView: 2.5,
-        },
+        // Add other breakpoints
       }}
       className={`mySwiper ${styles.swiper}`}
     >

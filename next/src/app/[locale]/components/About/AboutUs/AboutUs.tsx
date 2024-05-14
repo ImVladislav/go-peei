756178e15d17adc606be7a466bdfc4fce598baf9
vladislavzaver91 <vladislavzaver91@gmail.com>
2024-05-14 12:57:48 +0300

    fix: fixed about pages, corrected remark

--- next/src/app/[locale]/components/About/AboutUs/AboutUs.tsx
+++ next/src/app/[locale]/components/About/AboutUs/AboutUs.tsx
@@ -9,20 +9,22 @@ import styles from './aboutUs.module.scss'
 const AboutUs = () => {
 	return (
 		<section className={styles.section}>
-			<div className='container'>
-				<Title>
-					<Translator>about</Translator>
-					<span className={styles.accentTitle}>
-						{' '}
-						<Translator>us</Translator>
-					</span>
-				</Title>
-				<p className={styles.text}>
-					<span className={styles.accent_text}>
-						<Translator>PlatformForInitiatives</Translator>
-					</span>{' '}
-					<Translator>aboutUsPrimaryText</Translator>
-				</p>
+			<div className={styles.sectionWrapper}>
+				<div className='container'>
+					<Title>
+						<Translator>about</Translator>
+						<span className={styles.accentTitle}>
+							{' '}
+							<Translator>us</Translator>
+						</span>
+					</Title>
+					<p className={styles.text}>
+						<span className={styles.accent_text}>
+							<Translator>PlatformForInitiatives</Translator>
+						</span>{' '}
+						<Translator>aboutUsPrimaryText</Translator>
+					</p>
+				</div>
 
 				{/* mobile */}
 				<div className={`${styles.imgWrapper} ${styles.mobile}`}>
@@ -34,31 +36,33 @@ const AboutUs = () => {
 					/>
 				</div>
 				{/* desktop */}
-				<div className={`${styles.imgWrapper} ${styles.desktop}`}>
-					<Image
-						src='/about/about_bgimg.png'
-						alt='leafs'
-						fill
-						className={styles.bg_img}
-					/>
-				</div>
+				<div className='container'>
+					<div className={`${styles.imgWrapper} ${styles.desktop}`}>
+						<Image
+							src='/about/about_bgimg.png'
+							alt='leafs'
+							fill
+							className={styles.bg_img}
+						/>
+					</div>
 
-				<ul className={styles.list}>
-					{ABOUT_US.map((item, index) => (
-						<li key={index} className={styles.item}>
-							<Image
-								src={item.icon}
-								alt='Icon'
-								width={13}
-								height={22}
-								className={styles.item__icon}
-							/>
-							<p className={styles.item__desc}>
-								<Translator>{item.desc}</Translator>
-							</p>
-						</li>
-					))}
-				</ul>
+					<ul className={styles.list}>
+						{ABOUT_US.map((item, index) => (
+							<li key={index} className={styles.item}>
+								<Image
+									src={item.icon}
+									alt='Icon'
+									width={13}
+									height={22}
+									className={styles.item__icon}
+								/>
+								<p className={styles.item__desc}>
+									<Translator>{item.desc}</Translator>
+								</p>
+							</li>
+						))}
+					</ul>
+				</div>
 			</div>
 		</section>
 	)
--- next/src/app/[locale]/components/About/Values/Values.tsx
+++ next/src/app/[locale]/components/About/Values/Values.tsx
@@ -9,32 +9,34 @@ import styles from './values.module.scss'
 const Values = () => {
 	return (
 		<section className={styles.section}>
-			<div className='container'>
-				<Title>
-					<Translator>ours</Translator>
-					<span className={styles.accentTitle}>
-						{' '}
-						<Translator>values</Translator>
-					</span>
-				</Title>
-			</div>
-			<div className={styles.parallax}>
-				<ul className={styles.list}>
-					{VALUES.map((item, index) => (
-						<li key={index} className={styles.item}>
-							<Image
-								src={item.icon}
-								alt='icon'
-								width={55}
-								height={48}
-								className={styles.item__icon}
-							/>
-							<p className={styles.item__desc}>
-								<Translator>{item.desc}</Translator>
-							</p>
-						</li>
-					))}
-				</ul>
+			<div className={styles.sectionWrapper}>
+				<div className='container'>
+					<Title>
+						<Translator>ours</Translator>
+						<span className={styles.accentTitle}>
+							{' '}
+							<Translator>values</Translator>
+						</span>
+					</Title>
+				</div>
+				<div className={styles.parallax}>
+					<ul className={styles.list}>
+						{VALUES.map((item, index) => (
+							<li key={index} className={styles.item}>
+								<Image
+									src={item.icon}
+									alt='icon'
+									width={55}
+									height={48}
+									className={styles.item__icon}
+								/>
+								<p className={styles.item__desc}>
+									<Translator>{item.desc}</Translator>
+								</p>
+							</li>
+						))}
+					</ul>
+				</div>
 			</div>
 		</section>
 	)

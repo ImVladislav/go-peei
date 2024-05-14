e3455fd4b74e7cf6dec0a7b538d1ddc8b2c5a98e
ImVladislav <nafanya102@gmail.com>
2024-05-14 18:03:00 +0300

    a

--- next/src/app/api/auth/[...nextauth]/route.js
+++ next/src/app/api/auth/[...nextauth]/route.js
@@ -4,7 +4,7 @@ import db from "../../../../../libs/db";
 import User from "../../../../../models/UserModel";
 import bcrypt from "bcrypt";
 import { NextResponse } from "next/server";
-
+import { authOptions } from "./authOptions";
 async function login(credentials) {
   try {
     await db.connect();
@@ -28,57 +28,5 @@ async function login(credentials) {
   // }
 }
 
-// export const authOptions = {
-//   pages: {
-//     signIn: "/login",
-//   },
-//   providers: [
-//     CredentialsProvider({
-//       name: "credentials",
-//       credentials: {
-//         email: { label: "Email", type: "text", placeholder: "jsmith" },
-//         password: { label: "Password", type: "password" },
-//       },
-
-//       async authorize(credentials) {
-//         try {
-//           const user = await login(credentials);
-//           // console.log({ credentials });
-//           // console.log("this is user=", user);
-//           return user;
-//         } catch (error) {
-//           throw new Error("Failed to login");
-//         }
-//         // const user = await User.findOne({ email: credentials?.email });
-//         // if (!user) {
-//         //   throw new Error("No such user found!");
-//         // }
-//       },
-//     }),
-//   ],
-//   callbacks: {
-//     async jwt(token, user) {
-//       if (user) {
-//         token.id = user.id;
-//         token.email = user.email;
-//         token.username = user.username;
-//       }
-//       console.log("token=", token);
-//       return token;
-//     },
-//     async session(session, token) {
-//       if (token) {
-//         session.user.id = token.id;
-//         session.user.email = token.email;
-//         session.user.username = token.username;
-//       } else {
-//         console.log("token проєбали");
-//       }
-//       console.log("session=", session);
-//       return session;
-//     },
-//   },
-// };
-
-// const handler = NextAuth(authOptions);
-// export { handler as GET, handler as POST };
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };

afcac485679353f844c075baa8a8c404686b94c7
ImVladislav <nafanya102@gmail.com>
2024-05-14 17:55:06 +0300

    Update route.js

--- next/src/app/api/auth/[...nextauth]/route.js
+++ next/src/app/api/auth/[...nextauth]/route.js
@@ -28,57 +28,57 @@ async function login(credentials) {
   // }
 }
 
-export const authOptions = {
-  pages: {
-    signIn: "/login",
-  },
-  providers: [
-    CredentialsProvider({
-      name: "credentials",
-      credentials: {
-        email: { label: "Email", type: "text", placeholder: "jsmith" },
-        password: { label: "Password", type: "password" },
-      },
+// export const authOptions = {
+//   pages: {
+//     signIn: "/login",
+//   },
+//   providers: [
+//     CredentialsProvider({
+//       name: "credentials",
+//       credentials: {
+//         email: { label: "Email", type: "text", placeholder: "jsmith" },
+//         password: { label: "Password", type: "password" },
+//       },
 
-      async authorize(credentials) {
-        try {
-          const user = await login(credentials);
-          // console.log({ credentials });
-          // console.log("this is user=", user);
-          return user;
-        } catch (error) {
-          throw new Error("Failed to login");
-        }
-        // const user = await User.findOne({ email: credentials?.email });
-        // if (!user) {
-        //   throw new Error("No such user found!");
-        // }
-      },
-    }),
-  ],
-  callbacks: {
-    async jwt(token, user) {
-      if (user) {
-        token.id = user.id;
-        token.email = user.email;
-        token.username = user.username;
-      }
-      console.log("token=", token);
-      return token;
-    },
-    async session(session, token) {
-      if (token) {
-        session.user.id = token.id;
-        session.user.email = token.email;
-        session.user.username = token.username;
-      } else {
-        console.log("token проєбали");
-      }
-      console.log("session=", session);
-      return session;
-    },
-  },
-};
+//       async authorize(credentials) {
+//         try {
+//           const user = await login(credentials);
+//           // console.log({ credentials });
+//           // console.log("this is user=", user);
+//           return user;
+//         } catch (error) {
+//           throw new Error("Failed to login");
+//         }
+//         // const user = await User.findOne({ email: credentials?.email });
+//         // if (!user) {
+//         //   throw new Error("No such user found!");
+//         // }
+//       },
+//     }),
+//   ],
+//   callbacks: {
+//     async jwt(token, user) {
+//       if (user) {
+//         token.id = user.id;
+//         token.email = user.email;
+//         token.username = user.username;
+//       }
+//       console.log("token=", token);
+//       return token;
+//     },
+//     async session(session, token) {
+//       if (token) {
+//         session.user.id = token.id;
+//         session.user.email = token.email;
+//         session.user.username = token.username;
+//       } else {
+//         console.log("token проєбали");
+//       }
+//       console.log("session=", session);
+//       return session;
+//     },
+//   },
+// };
 
-const handler = NextAuth(authOptions);
-export { handler as GET, handler as POST };
+// const handler = NextAuth(authOptions);
+// export { handler as GET, handler as POST };

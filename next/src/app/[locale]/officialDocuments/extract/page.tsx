91ae30852b0e903c8d603dfbd7236ab34d20eba7
ImVladislav <nafanya102@gmail.com>
2024-05-17 18:01:05 +0300

    Update page.tsx

--- next/src/app/[locale]/officialDocuments/extract/page.tsx
+++ next/src/app/[locale]/officialDocuments/extract/page.tsx
@@ -1,13 +1,25 @@
-import React from "react";
-import { getDataId } from "../../../../../libs/getData";
+"use client";
+import { useEffect, useState } from "react";
 
 interface docsType {
   src: string;
 }
+const Extract = () => {
+  const [data, setData] = useState<docsType | null>(null);
 
-const Extract = async () => {
-  const data: docsType = await getDataId("docs", "65fd54a7831c00f0aebbb403");
-  console.log(data.src);
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch("/api/docs/65fd54a7831c00f0aebbb403");
+      const result = await response.json();
+      setData(result);
+    };
+
+    fetchData();
+  }, []);
+
+  if (!data) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <div className="container">
@@ -15,20 +27,36 @@ const Extract = async () => {
         style={{ width: "100%", height: "100vh" }}
         src={data.src}
         frameBorder="0"
-        full-frame=""
         allowFullScreen
       ></iframe>
-
-      {/* <embed
-        type="application/x-google-chrome-pdf"
-        src="chrome-extension://mhjfbmdgcfjbbpaeojofohoefgiehjai/fab508df-a4ba-420c-9f81-7727d0840c8e"
-        original-url="https://www.uny.org.ua/wp-content/uploads/2023/03/statut-go-ty-potriben-ukrayini.pdf"
-        background-color="4283586137"
-        javascript="allow"
-        full-frame=""
-      ></embed> */}
     </div>
   );
 };
 
 export default Extract;
+
+// import React from "react";
+// import { getDataId } from "../../../../../libs/getData";
+
+// interface docsType {
+//   src: string;
+// }
+
+// const Extract = async () => {
+//   const data: docsType = await getDataId("docs", "65fd54a7831c00f0aebbb403");
+//   console.log(data.src);
+
+//   return (
+//     <div className="container">
+//       <iframe
+//         style={{ width: "100%", height: "100vh" }}
+//         src={data.src}
+//         frameBorder="0"
+//         full-frame=""
+//         allowFullScreen
+//       ></iframe>
+//     </div>
+//   );
+// };
+
+// export default Extract;

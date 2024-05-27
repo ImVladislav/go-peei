61f5d6d11fa11091c0cb534a6460007023558eb5
vladislavzaver91 <vladislavzaver91@gmail.com>
2024-05-27 16:51:12 +0300

    fix: fixed loader & news item page

--- next/src/app/[locale]/components/Loader/Loader.tsx
+++ next/src/app/[locale]/components/Loader/Loader.tsx
@@ -2,18 +2,24 @@
 import Image from 'next/image'
 import styles from './loader.module.scss'
 
-interface ILoaderProps { 
-  newStyles?: string,
-  width?: number,
-  height?: number 
+interface ILoaderProps {
+	newStyles?: string
+	width?: number
+	height?: number
 }
 
-const Loader = ({width, height, newStyles}: ILoaderProps) => {
-  return (
-    <div className={styles.spinner}>
-      <Image src='/loader/leaf-loader.svg' alt='loader' width={width} height={height} className={`${styles.icon} ${newStyles}`}/>
-    </div>
-  )
-};
+const Loader = ({ width, height, newStyles }: ILoaderProps) => {
+	return (
+		<div className={styles.spinner}>
+			<Image
+				src='/loader/leaf-loader.svg'
+				alt='loader'
+				width={width}
+				height={height}
+				className={`${styles.icon} ${newStyles}`}
+			/>
+		</div>
+	)
+}
 
 export default Loader
--- next/src/app/[locale]/components/NewsItem/ReadMore/SingleNewsItem.tsx
+++ next/src/app/[locale]/components/NewsItem/ReadMore/SingleNewsItem.tsx
@@ -1,47 +1,48 @@
-import { newsItem } from "@/app/types";
-import Image from "next/image";
-import Link from "next/link";
-import { formatDate } from "../../formatDate";
-import styles from "./singleNewsItem.module.scss";
+import { newsItem } from '@/app/types'
+import Image from 'next/image'
+import Link from 'next/link'
+import { formatDate } from '../../formatDate'
+import styles from './singleNewsItem.module.scss'
 
 interface SingleNewsItemProps {
-  newsItem: newsItem;
-  newStyle?: string;
-  locale: string;
+	newsItem: newsItem
+	newStyle?: string
+	locale: string
 }
 
 const SingleNewsItem = async ({
-  newsItem,
-  newStyle,
-  locale,
+	newsItem,
+	newStyle,
+	locale,
 }: SingleNewsItemProps) => {
-  return (
-    <>
-      <li key={newsItem._id} className={`${styles.newsItem} ${newStyle}`}>
-        <div className={styles.newsItem__wrap}>
-          <Link
-            href={`/news/${newsItem._id}`}
-            className={styles.newsItem__titleLink}
-          >
-            <h3 className={styles.newsItem__title}>
-              {locale === "uk" ? newsItem.title : newsItem.titleEn}
-            </h3>
-          </Link>
-          <div className={styles.newsItem__imgBox}>
-            <Image
-              src={newsItem.imageSrc}
-              alt={newsItem.title}
-              fill
-              className={styles.newsItem__img}
-            />
-          </div>
-        </div>
-        <p className={styles.newsItem__date}>
-          {formatDate(newsItem.createdAt)}
-        </p>
-      </li>
-    </>
-  );
-};
+	return (
+		<>
+			<li key={newsItem._id} className={`${styles.newsItem} ${newStyle}`}>
+				<Link
+					href={`/news/${newsItem._id}`}
+					className={styles.newsItem__titleLink}
+				>
+					<div className={styles.newsItem__wrap}>
+						<h3 className={styles.newsItem__title}>
+							{locale === 'uk' ? newsItem.title : newsItem.titleEn}
+						</h3>
 
-export default SingleNewsItem;
+						<div className={styles.newsItem__imgBox}>
+							<Image
+								src={newsItem.imageSrc}
+								alt={newsItem.title}
+								fill
+								className={styles.newsItem__img}
+							/>
+						</div>
+					</div>
+					<p className={styles.newsItem__date}>
+						{formatDate(newsItem.createdAt)}
+					</p>
+				</Link>
+			</li>
+		</>
+	)
+}
+
+export default SingleNewsItem

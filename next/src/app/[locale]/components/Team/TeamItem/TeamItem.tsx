3bbcf4d9f1af84e52ec5ffd76be21ab6bc15d426
Irka-Bodnariuk <bodnariuk.iryna1@gmail.com>
2024-05-28 20:25:48 +0300

    fix: change email and styles team page

--- next/src/app/[locale]/components/Team/TeamItem/TeamItem.tsx
+++ next/src/app/[locale]/components/Team/TeamItem/TeamItem.tsx
@@ -1,50 +1,56 @@
-import Image from 'next/image'
-import Translator from '../../translator/Translator'
-import styles from './teamItem.module.scss'
+import Image from "next/image";
+import Translator from "../../translator/Translator";
+import styles from "./teamItem.module.scss";
+import { iconsData } from "@/app/constants";
+import LinkComponent from "../../LinkComponent/LinkComponent";
 
 interface itemProps {
-	item: {
-		image: string
-		name: string
-		about: string
-		position: string
-		email?: string
-	}
+  item: {
+    image: string;
+    name: string;
+    about: string;
+    position: string;
+    email?: string;
+  };
 }
 
 const TeamItem = ({ item }: itemProps, key: string) => {
-	return (
-		<>
-			<li className={styles.item}>
-				<div className={styles.item__container}>
-					<div className={styles.item__imgWrapper}>
-						<Image
-							src={item.image}
-							alt={item.name}
-							fill
-							className={styles.item__img}
-						/>
-					</div>
-					<div className={styles.item__infoWrapper}>
-						<h3 className={styles.item__title}>
-							<Translator>{item.name}</Translator>
-						</h3>
-						<p className={styles.item__position}>
-							<Translator>{item.position}</Translator>
-						</p>
-						<div className={styles.item__emailWrapper}>
-							<Image src='/team/Email.svg' alt='email' width={24} height={24} />
-							<p className={styles.item__email}>{item.email}</p>
-						</div>
-					</div>
-				</div>
+  return (
+    <>
+      <li className={styles.item}>
+        <div className={styles.item__container}>
+          <div className={styles.item__imgWrapper}>
+            <Image
+              src={item.image}
+              alt={item.name}
+              fill
+              className={styles.item__img}
+            />
+          </div>
+          <div className={styles.item__infoWrapper}>
+            <h3 className={styles.item__title}>
+              <Translator>{item.name}</Translator>
+            </h3>
+            <p className={styles.item__position}>
+              <Translator>{item.position}</Translator>
+            </p>
 
-				<p className={styles.item__about}>
-					<Translator>{item.about}</Translator>
-				</p>
-			</li>
-		</>
-	)
-}
+            <LinkComponent
+              iconsData={iconsData}
+              iconName={item.email}
+              href={`mailto:${item.email}`}
+              name="Email"
+              newStyles={styles.item__email}
+            />
+          </div>
+        </div>
 
-export default TeamItem
+        <p className={styles.item__about}>
+          <Translator>{item.about}</Translator>
+        </p>
+      </li>
+    </>
+  );
+};
+
+export default TeamItem;
--- next/src/app/[locale]/components/Team/TeamList/team-list.data.ts
+++ next/src/app/[locale]/components/Team/TeamList/team-list.data.ts
@@ -1,37 +1,37 @@
 export const TEAM = [
-	{
-		image: '/team/Glagyshko_S.jpg',
-		name: 'serhiiHladyshko',
-		position: 'founderOfTheOrganization',
-		email: 'copeeiua@gmail.com',
-		about: 'aboutSerhiiHladyshko',
-	},
-	{
-		image: '/team/Bezkorovainy_Y.jpg',
-		name: 'yevgenyBezkorovainy',
-		position: 'deputyChairman',
-		email: 'copeeiua@gmail.com',
-		about: 'aboutYevgenyBezkorovainy',
-	},
-	{
-		image: '/team/Gordienko_I.jpg',
-		name: 'ilonaGordienko',
-		position: 'memberOfTheBoard',
-		email: 'copeeiua@gmail.com',
-		about: 'aboutIlonaGordienko',
-	},
-	{
-		image: '/team/Bezkorovaina_T.jpg',
-		name: 'tatianaBezkorovaina',
-		position: 'memberOfTheBoard',
-		email: 'copeeiua@gmail.com',
-		about: 'aboutTatianaBezkorovaina',
-	},
-	{
-		image: '/team/Gordienko_M.jpg',
-		name: 'mykolaGordienko',
-		position: 'projectManager',
-		email: 'copeeiua@gmail.com',
-		about: 'aboutMykolaGordienko',
-	},
-]
+  {
+    image: "/team/Glagyshko_S.jpg",
+    name: "serhiiHladyshko",
+    position: "founderOfTheOrganization",
+    email: "copeeiua@gmail.com",
+    about: "aboutSerhiiHladyshko",
+  },
+  {
+    image: "/team/Bezkorovainy_Y.jpg",
+    name: "yevgenyBezkorovainy",
+    position: "deputyChairman",
+    email: "copeeiua@gmail.com",
+    about: "aboutYevgenyBezkorovainy",
+  },
+  {
+    image: "/team/Gordienko_I.jpg",
+    name: "ilonaGordienko",
+    position: "memberOfTheBoard",
+    email: "copeeiua@gmail.com",
+    about: "aboutIlonaGordienko",
+  },
+  {
+    image: "/team/Bezkorovaina_T.jpg",
+    name: "tatianaBezkorovaina",
+    position: "memberOfTheBoard",
+    email: "copeeiua@gmail.com",
+    about: "aboutTatianaBezkorovaina",
+  },
+  {
+    image: "/team/Gordienko_M.jpg",
+    name: "mykolaGordienko",
+    position: "projectManager",
+    email: "copeeiua@gmail.com",
+    about: "aboutMykolaGordienko",
+  },
+];

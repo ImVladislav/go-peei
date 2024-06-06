3625df09a32772d232d95dd9523cbb4e59947445
ImVladislav <nafanya102@gmail.com>
2024-06-06 17:36:00 +0300

    add partners

---
+++ next/src/app/[locale]/partners/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Title from "../components/Title/Title";
+import Translator from "../components/translator/Translator";
+const OurPartners = () => {
+  return (
+    <div className="container">
+      <Title>
+        <Translator>ourPartners</Translator>
+      </Title>
+    </div>
+  );
+};
+
+export default OurPartners;

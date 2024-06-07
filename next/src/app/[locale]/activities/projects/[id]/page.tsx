ed62e5a3f0bf0dcb17f1c193a864ced740a5bb54
Irka-Bodnariuk <bodnariuk.iryna1@gmail.com>
2024-06-07 13:22:05 +0300

    fix: change projects pages

--- next/src/app/[locale]/activities/projects/[id]/page.tsx
+++ next/src/app/[locale]/activities/projects/[id]/page.tsx
@@ -42,103 +42,119 @@ const ProjectDetails = async ({
                   "{locale === "uk" ? data.title : data.titleEn}"
                 </span>
               </Title>
-              <div className={styles.imgContainer}>
-                <div className={styles.imgInner}>
-                  <Image
-                    className={styles.img}
-                    src={data.imageSrc}
-                    alt={data.title}
-                    fill
-                  />
-                </div>
-              </div>
-            </div>
 
-            <div className={styles.card}>
-              <div className={styles.progres_bar}>
-                <div className={styles.progres_bar_box}>
-                  <div
-                    className={styles.progres_bar_fill}
-                    style={{ width: `${progress}%`, background: "#0d490d" }}
-                  ></div>
+              <div className={styles.containerCard}>
+                <div className={styles.imgContainer}>
+                  <div className={styles.imgInner}>
+                    <Image
+                      className={styles.img}
+                      src={data.imageSrc}
+                      alt={data.title}
+                      fill
+                    />
+                  </div>
                 </div>
-                <div className={styles.progres_bar_text}>
-                  <div className={styles.progres_bar_inner}>
-                    <p className={styles.progres_bar_label}>
-                      <Translator>collected</Translator>
-                    </p>
-                    <p className={styles.progres_bar_money}>
-                      {formatPrice(data.fundsRaised)} грн
-                    </p>
+                <div className={styles.card}>
+                  <div className={styles.progres_bar}>
+                    <div className={styles.progres_bar_box}>
+                      <div
+                        className={styles.progres_bar_fill}
+                        style={{ width: `${progress}%`, background: "#0d490d" }}
+                      ></div>
+                    </div>
+                    <div className={styles.progres_bar_text}>
+                      <div className={styles.progres_bar_inner}>
+                        <p className={styles.progres_bar_label}>
+                          <Translator>collected</Translator>
+                        </p>
+                        <p className={styles.progres_bar_money}>
+                          {formatPrice(data.fundsRaised)}
+                          {locale === "uk" ? " грн" : " uah"}
+                        </p>
+                      </div>
+                      <div className={styles.progres_bar_inner}>
+                        <p className={styles.progres_bar_label}>
+                          <Translator>totalAmount</Translator>
+                        </p>
+                        <p className={styles.progres_bar_money}>
+                          {formatPrice(data.total)}
+                          {locale === "uk" ? " грн" : " uah"}
+                        </p>
+                      </div>
+                    </div>
                   </div>
-                  <div className={styles.progres_bar_inner}>
-                    <p className={styles.progres_bar_label}>
-                      <Translator>totalAmount</Translator>
-                    </p>
-                    <p className={styles.progres_bar_money}>
-                      {formatPrice(data.total)} грн
-                    </p>
-                  </div>
+                  <Button
+                    newStyles={styles.btn}
+                    isLink
+                    href={data.link}
+                    closed={data.closed}
+                  >
+                    <Translator>
+                      {data.closed ? "donateСlosed" : "donate"}
+                    </Translator>
+                  </Button>
                 </div>
               </div>
-              <Button
-                newStyles={styles.btn}
-                isLink
-                href="https://payhub.com.ua/#/service/98840"
-              >
-                <Translator>donate</Translator>
-              </Button>
             </div>
           </div>
-          <div className={styles.containerCol}>
+          {/* <div className={styles.containerCol}> */}
+          <div className={styles.container}>
+            <h3 className={styles.title}>
+              <Translator>aboutTheProject</Translator>
+            </h3>
+            <p className={styles.description}>
+              "{locale === "uk" ? data.description : data.descriptionEn}"
+            </p>
+          </div>
+          {data.report && (
             <div className={styles.container}>
-              <h3 className={styles.title}>
-                <Translator>aboutTheProject</Translator>
-              </h3>
-              <p className={styles.description}>
-                "{locale === "uk" ? data.description : data.descriptionEn}"
-              </p>
-            </div>
-            {data.report && (
-              <div className={styles.container}>
-                <h3 className={styles.title}>
-                  <Translator>projectReporting</Translator>
-                </h3>
-                <div className={styles.reportWrap}>
-                  {/* {data.report && ( */}
+              <div className={styles.reportWrap}>
+                {/* {data.report && ( */}
+
+                <div className={styles.container}>
+                  <h3 className={styles.title}>
+                    <Translator>projectReporting</Translator>
+                  </h3>
+                </div>
+                <div className={styles.container}>
                   <Link href={data.report} className={styles.report}>
                     <Translator>projectReport</Translator>
                   </Link>
-                  {/* )} */}
                 </div>
+                {/* )} */}
               </div>
-            )}
-          </div>
-
-          {data.photoReport?.length !== 0 && (
-            <div className={styles.container}>
-              <h3 className={styles.title}>
-                <Translator>photoReport</Translator>
-              </h3>
-              {data.photoReport?.length === 1 ? (
-                <div className={styles.photo_imgWrap}>
-                  <Image
-                    className={styles.photo_img}
-                    src={data.photoReport[0]}
-                    alt="photo"
-                    fill
-                  />
-                </div>
-              ) : (
-                data.photoReport && <PhotoSwiper data={data.photoReport} />
-              )}
             </div>
           )}
+          {/* </div> */}
 
-          <div className={styles.box}>
-            <Link className={styles.box__link} href="/activities/projects">
-              <Translator>backToProjects</Translator>
-            </Link>
+          <div className={styles.containerBottom}>
+            {data.photoReport?.length !== 0 && (
+              <div className={styles.containerPhoto}>
+                <div className={styles.container}>
+                  <h3 className={styles.title}>
+                    <Translator>photoReport</Translator>
+                  </h3>
+                </div>
+                {data.photoReport?.length === 1 ? (
+                  <div className={styles.photo_imgWrap}>
+                    <Image
+                      className={styles.photo_img}
+                      src={data.photoReport[0]}
+                      alt="photo"
+                      fill
+                    />
+                  </div>
+                ) : (
+                  data.photoReport && <PhotoSwiper data={data.photoReport} />
+                )}
+              </div>
+            )}
+
+            <div className={styles.box}>
+              <Link className={styles.box__link} href="/activities/projects">
+                <Translator>backToProjects</Translator>
+              </Link>
+            </div>
           </div>
         </div>
       </div>

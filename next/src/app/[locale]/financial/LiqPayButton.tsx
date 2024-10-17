a4be0543e2ed1e7aa6c8c281a1fc652459931c16
vladislavzaver91 <vladislavzaver91@gmail.com>
2024-10-17 17:40:16 +0300

    feat: add 1st phase of work - modified schoolEnergy page

--- next/src/app/[locale]/financial/LiqPayButton.tsx
+++ next/src/app/[locale]/financial/LiqPayButton.tsx
@@ -1,84 +1,78 @@
-"use client";
-import React, { useState } from "react";
-import CryptoJS from "crypto-js";
-import s from "./Financial.module.scss";
-import { useTranslation } from "react-i18next";
-import LiqpayButtonImage from "../../../../public/financial/liqpayLogo.png";
+'use client'
+import CryptoJS from 'crypto-js'
+import React from 'react'
+import { useTranslation } from 'react-i18next'
+import s from './Financial.module.scss'
 
 interface LiqPayButtonProps {
-  public_key: any;
-  private_key: any;
-  amount: number;
-  description: string;
-  currency: string;
+	public_key: any
+	private_key: any
+	amount: number
+	description: string
+	currency: string
 }
 
 const LiqPayButton: React.FC<LiqPayButtonProps> = ({
-  public_key,
-  private_key,
-  amount,
-  description,
-  currency,
+	public_key,
+	private_key,
+	amount,
+	description,
+	currency,
 }) => {
-  // const [showSuccessModal, setShowSuccessModal] = useState(false);
-  // const [showErrorModal, setShowErrorModal] = useState(false);
-  // const [showModal, setShowModal] = useState(false);
-  const { t, i18n } = useTranslation(); // Destructure the i18n object from useTranslation
-  // Get the current language
-  const currentLanguage = i18n.language;
-  // console.log(currency);
+	// const [showSuccessModal, setShowSuccessModal] = useState(false);
+	// const [showErrorModal, setShowErrorModal] = useState(false);
+	// const [showModal, setShowModal] = useState(false);
+	const { t, i18n } = useTranslation() // Destructure the i18n object from useTranslation
+	// Get the current language
+	const currentLanguage = i18n.language
+	// console.log(currency);
 
-  const generateLiqPayData = (params: Record<string, any>) => {
-    const jsonString = JSON.stringify(params);
-    const base64Data = Buffer.from(jsonString).toString("base64");
-    const signature = CryptoJS.SHA1(
-      private_key + base64Data + private_key
-    ).toString(CryptoJS.enc.Base64);
-    return { data: base64Data, signature };
-  };
+	const generateLiqPayData = (params: Record<string, any>) => {
+		const jsonString = JSON.stringify(params)
+		const base64Data = Buffer.from(jsonString).toString('base64')
+		const signature = CryptoJS.SHA1(
+			private_key + base64Data + private_key
+		).toString(CryptoJS.enc.Base64)
+		return { data: base64Data, signature }
+	}
 
-  // const handlePaymentSuccess = () => {
-  //   setShowSuccessModal(true);
-  //   setShowModal(true);
-  //   console.log("success");
-  // };
+	// const handlePaymentSuccess = () => {
+	//   setShowSuccessModal(true);
+	//   setShowModal(true);
+	//   console.log("success");
+	// };
 
-  // const handlePaymentError = () => {
-  //   setShowErrorModal(true);
-  //   console.log("error");
-  // };
+	// const handlePaymentError = () => {
+	//   setShowErrorModal(true);
+	//   console.log("error");
+	// };
 
-  const params = {
-    version: 3,
-    public_key,
-    action: "paydonate",
-    amount,
-    currency: `${currency}`,
-    description,
-    order_id: `order_id_${Date.now()}`,
-    paytypes: "apay,gpay,card,privat24,invoice,qr",
-    format: "json",
-    language: `${currentLanguage}`,
-  };
+	const params = {
+		version: 3,
+		public_key,
+		action: 'paydonate',
+		amount,
+		currency: `${currency}`,
+		description,
+		order_id: `order_id_${Date.now()}`,
+		paytypes: 'apay,gpay,card,privat24,invoice,qr',
+		format: 'json',
+		language: `${currentLanguage}`,
+	}
 
-  const { data, signature } = generateLiqPayData(params);
+	const { data, signature } = generateLiqPayData(params)
 
-  return (
-    <form
-      method="POST"
-      action="https://www.liqpay.ua/api/3/checkout"
-      acceptCharset="utf-8"
-    >
-      <input type="hidden" name="data" value={data} />
-      <input type="hidden" name="signature" value={signature} />
-      <input
-        className={s.financial__btnBlock}
-        type="image"
-        src={LiqpayButtonImage.src}
-        alt="Pay"
-      />
-    </form>
-  );
-};
+	return (
+		<form
+			method='POST'
+			action='https://www.liqpay.ua/api/3/checkout'
+			acceptCharset='utf-8'
+		>
+			<input type='hidden' name='data' value={data} />
+			<input type='hidden' name='signature' value={signature} />
+			<button className={s.btnPay}>Підтримати</button>
+		</form>
+	)
+}
 
-export default LiqPayButton;
+export default LiqPayButton
--- next/src/app/[locale]/financial/schoolEnergy/page.tsx
+++ next/src/app/[locale]/financial/schoolEnergy/page.tsx
@@ -1,195 +1,214 @@
-"use client";
-import React, { useState } from "react";
-import LiqPayButton from "../LiqPayButton";
-import s from "../Financial.module.scss";
+'use client'
+import React, { useState } from 'react'
+import s from '../Financial.module.scss'
+import LiqPayButton from '../LiqPayButton'
 
-import { useTranslation } from "react-i18next";
-import Translator from "../../components/translator/Translator";
-import Link from "next/link";
-import Title from "../../components/Title/Title";
+import Link from 'next/link'
+import { useTranslation } from 'react-i18next'
+import Title from '../../components/Title/Title'
+import Translator from '../../components/translator/Translator'
 
-import CopySvg from "../../../../../public/financial/copy.svg";
-import Image from "next/image";
+import Image from 'next/image'
+import CopySvg from '../../../../../public/financial/copy.svg'
 
 const FinancialPage: React.FC = () => {
-  const [donationAmount, setDonationAmount] = useState(20);
-  const [currency, setCurrency] = useState("UAH");
-  const { t, i18n } = useTranslation();
-  const currentLanguage = i18n.language;
-  const [selectedCurrency, setSelectedCurrency] = useState(
-    currentLanguage === "uk" ? "UAH" : "USD"
-  );
+	const [donationAmount, setDonationAmount] = useState(20)
+	const [currency, setCurrency] = useState('UAH')
+	const { t, i18n } = useTranslation()
+	const currentLanguage = i18n.language
+	const [selectedCurrency, setSelectedCurrency] = useState(
+		currentLanguage === 'uk' ? 'UAH' : 'USD'
+	)
 
-  const handleAmountButtonClick = (amount: number) => {
-    setDonationAmount(amount);
-  };
+	const handleAmountButtonClick = (amount: number) => {
+		setDonationAmount(amount)
+	}
 
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const value = event.target.value;
-    const parsedValue = parseFloat(value);
+	const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		const value = event.target.value
+		const parsedValue = parseFloat(value)
 
-    if (!isNaN(parsedValue) && parsedValue >= 20) {
-      setDonationAmount(parsedValue);
-    } else if (parsedValue < 20) {
-      setDonationAmount(20);
-    }
-  };
+		if (!isNaN(parsedValue) && parsedValue >= 20) {
+			setDonationAmount(parsedValue)
+		} else if (parsedValue < 20) {
+			setDonationAmount(20)
+		}
+	}
 
-  const handleCurrencyChange = (
-    event: React.ChangeEvent<HTMLSelectElement>
-  ) => {
-    setSelectedCurrency(event.target.value);
-  };
+	const handleCurrencyChange = (
+		event: React.ChangeEvent<HTMLSelectElement>
+	) => {
+		setSelectedCurrency(event.target.value)
+	}
 
-  const [fields] = useState([
-    {
-      key: "accountNumberFormat",
-      label: t("accountNumberFormat"),
-      value: t("platformEcoEnergyInitiatives"),
-      id: "account",
-    },
-    {
-      key: "recipient",
-      label: t("recipient"),
-      value: t("publicOrganizationPlatformEcoEnergy"),
-      id: "client",
-    },
-    {
-      key: "iban",
-      label: t("iban"),
-      value: t("ibanNumber"),
-      id: "iban",
-    },
-    {
-      key: "edrpou",
-      label: t("edrpou"),
-      value: t("edrpouNumber"),
-      id: "edrpou",
-    },
-    {
-      key: "purposeOfPayment",
-      label: t("purposeOfPayment"),
-      value: t("charitydonation"),
-      id: "purpose",
-    },
-  ]);
+	const [fields] = useState([
+		// {
+		// 	key: 'accountNumberFormat',
+		// 	label: t('accountNumberFormat'),
+		// 	value: t('platformEcoEnergyInitiatives'),
+		// 	id: 'account',
+		// },
+		{
+			key: 'recipient',
+			label: t('recipient'),
+			value: t('publicOrganizationPlatformEcoEnergy'),
+			id: 'client',
+		},
+		{
+			key: 'iban',
+			label: t('iban'),
+			value: t('ibanNumber'),
+			id: 'iban',
+		},
+		{
+			key: 'edrpou',
+			label: t('edrpou'),
+			value: t('edrpouNumber'),
+			id: 'edrpou',
+		},
+		{
+			key: 'purposeOfPayment',
+			label: t('purposeOfPayment'),
+			value: t('charitydonation'),
+			id: 'purpose',
+		},
+	])
 
-  const copyToClipboard = (value: string) => {
-    navigator.clipboard
-      .writeText(value)
-      .then(() => {
-        console.log("Value copied to clipboard:", value);
-      })
-      .catch((error) => {
-        console.error("Failed to copy value to clipboard:", error);
-      });
-  };
+	const copyToClipboard = (value: string) => {
+		navigator.clipboard
+			.writeText(value)
+			.then(() => {
+				console.log('Value copied to clipboard:', value)
+			})
+			.catch(error => {
+				console.error('Failed to copy value to clipboard:', error)
+			})
+	}
 
-  const publicKey = process.env.NEXT_PUBLIC_PUBLIC_KEY;
-  const privateKey = process.env.NEXT_PUBLIC_PRIVATE_KEY;
+	const publicKey = process.env.NEXT_PUBLIC_PUBLIC_KEY
+	const privateKey = process.env.NEXT_PUBLIC_PRIVATE_KEY
 
-  return (
-    <div className={s.financial__bg}>
-      <div className={s.financial__container_mainBlock}>
-        <section className={s.financial__container}>
-          <Title>
-            <Translator>financialSupport</Translator>
-          </Title>
-          <div className={s.financial__container_mainBlock}>
-            <div className={s.financial__container_leftBlock}>
-              <h2 className={s.financial__subTitle}>
-                <Translator>selectContributionAmount</Translator>
-              </h2>
-              <div className="buttons">
-                <button
-                  className={s.financial__fixedAmountDonatBtn}
-                  onClick={() => handleAmountButtonClick(100.0)}
-                >
-                  100
-                </button>
-                <button
-                  className={s.financial__fixedAmountDonatBtn}
-                  onClick={() => handleAmountButtonClick(200.0)}
-                >
-                  200
-                </button>
-                <button
-                  className={s.financial__fixedAmountDonatBtn}
-                  onClick={() => handleAmountButtonClick(300.0)}
-                >
-                  300
-                </button>
-              </div>
-              <div className={s.amoontBlock}>
-                <input
-                  className={`${s.financial__fixedAmountDonatBtn} ${s.financial__fixedAmountDonatInput}`}
-                  type="text"
-                  pattern="\d*"
-                  placeholder={t("amount")}
-                  value={donationAmount.toString()}
-                  onChange={handleInputChange}
-                />
-                <select
-                  className={`${s.financial__fixedAmountDonatBtn} ${s.financial__fixedAmountDonatInput}`}
-                  style={{ width: "100px" }}
-                  value={selectedCurrency}
-                  onChange={handleCurrencyChange}
-                >
-                  <option className={s.option} value="UAH">
-                    UAH
-                  </option>
-                  <option className={s.option} value="USD">
-                    USD
-                  </option>
-                  <option className={s.option} value="EUR">
-                    EUR
-                  </option>
-                </select>
-              </div>
-              <div id="liqpay_checkout">
-                <LiqPayButton
-                  currency={selectedCurrency}
-                  public_key={publicKey}
-                  private_key={privateKey}
-                  amount={donationAmount}
-                  description={t("charitydonation")}
-                />
-              </div>
-              <Link
-                className={`${s.financial__subTitle} ${s.financial__link}`}
-                href="/officialDocuments/offert_contract"
-              >
-                {t("termsOfOffer")}
-              </Link>
-            </div>
-            <div className={s.financial__container_rightBlock}>
-              <h4 className={s.financial__subTitle}>
-                <Translator>BankDetails</Translator>
-              </h4>
-              <div className={s.financial__infoBlock}>
-                {fields.map((field) => (
-                  <div key={field.key} className={s.financial__infoField}>
-                    <h4 className={s.financial__subTitle}>{field.label}:</h4>
-                    <div className={s.financial__infoField__insideBlock}>
-                      <span className={s.financial__infoValue}>
-                        {field.value}
-                      </span>
-                      <button
-                        className={s.financial__copyButton}
-                        onClick={() => copyToClipboard(field.value)}
-                      >
-                        <Image src={CopySvg} alt={"Copy"}></Image>
-                      </button>
-                    </div>
-                  </div>
-                ))}
-              </div>
-            </div>
-          </div>
-        </section>
-      </div>
-    </div>
-  );
-};
+	return (
+		<div className='container'>
+			<section className={s.section}>
+				<div className={s.section__content}>
+					<div className={s.section__title}>
+						<Title>
+							<Translator>financialSupport</Translator>
+						</Title>
+					</div>
+					<div className={s.container_mainBlock}>
+						{/* left block; financial operations */}
+						<div className={s.container_leftBlock}>
+							<h2 className={s.title}>
+								<Translator>selectContributionAmount</Translator>
+							</h2>
+							<div className={s.amoontBlock}>
+								<div className={s.ammontBlock__inputWrapper}>
+									<input
+										className={s.fixedAmountDonatInput}
+										type='text'
+										pattern='\d*'
+										placeholder={t('amount')}
+										value={donationAmount.toString()}
+										onChange={handleInputChange}
+									/>
+									<p className={s.amoontBlock__recomendationText}>
+										Мінімальна сума внеску 20 грн
+									</p>
+								</div>
+								<select
+									className={`${s.fixedAmountDonatBtn} ${s.donatInputBtn}`}
+									value={selectedCurrency}
+									onChange={handleCurrencyChange}
+								>
+									<option className={s.amoontBlock__option} value='UAH'>
+										UAH
+									</option>
+									<option className={s.amoontBlock__option} value='USD'>
+										USD
+									</option>
+									<option className={s.amoontBlock__option} value='EUR'>
+										EUR
+									</option>
+								</select>
+							</div>
+							{/* ammount donate buttons */}
+							<div className='buttons'>
+								<h2 className={s.title}>Оберіть суму внеску</h2>
+								<button
+									className={`${s.fixedAmountDonatBtn} ${s.marginNone}`}
+									onClick={() => handleAmountButtonClick(100.0)}
+								>
+									100
+								</button>
+								<button
+									className={s.fixedAmountDonatBtn}
+									onClick={() => handleAmountButtonClick(200.0)}
+								>
+									200
+								</button>
+								<button
+									className={s.fixedAmountDonatBtn}
+									onClick={() => handleAmountButtonClick(300.0)}
+								>
+									300
+								</button>
+							</div>
+							{/* liqpay button */}
+							<div id='liqpay_checkout'>
+								<LiqPayButton
+									currency={selectedCurrency}
+									public_key={publicKey}
+									private_key={privateKey}
+									amount={donationAmount}
+									description={t('charitydonation')}
+								/>
+							</div>
+							{/* offer contract */}
+							<Link
+								className={s.offerLink}
+								href='/officialDocuments/offert_contract'
+							>
+								{t('termsOfOffer')}
+							</Link>
+						</div>
+						{/* right block; bank details */}
+						<div className={s.container_rightBlock}>
+							<h2 className={s.headTitle}>
+								<Translator>BankDetails</Translator>
+							</h2>
+							<div className={s.bankDetailsBlock}>
+								{fields.map(field => (
+									<div
+										key={field.key}
+										className={s.bankDetailsBlock__infoField}
+									>
+										<h3 className={s.bankDetailsBlock__subTitle}>
+											{field.label}:
+										</h3>
+										<div className={s.bankDetailsBlock__descriptionBlock}>
+											<p className={s.bankDetailsBlock__descriptionBlock__text}>
+												{field.value}
+											</p>
+											<button
+												className={
+													s.bankDetailsBlock__descriptionBlock__copyBtn
+												}
+												onClick={() => copyToClipboard(field.value)}
+											>
+												<Image src={CopySvg} alt={'Copy'}></Image>
+											</button>
+										</div>
+									</div>
+								))}
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+		</div>
+	)
+}
 
-export default FinancialPage;
+export default FinancialPage
